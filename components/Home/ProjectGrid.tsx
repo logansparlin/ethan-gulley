7aa3cc86dc5a9610511737ef20bc6280c8ce75c4
Logan Sparlin <logan.sparlin@gmail.com>
2022-09-02 16:42:54 -0400

    categories

--- components/Home/ProjectGrid.tsx
+++ components/Home/ProjectGrid.tsx
@@ -22,7 +22,7 @@ const ProjectGrid = ({ projects, category }) => {
           return (
             <StyledImage
               key={`${project._id}-${category}`}
-              initial={{ y: -30, opacity: 0 }}
+              initial={{ y: -20, opacity: 0 }}
               animate={{
                 y: 0, opacity: 1, transition: {
                   duration: 1.4,
