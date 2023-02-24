1af3335467a84236b715a43b25efe4a3ee4239f6
Logan Sparlin <logan.sparlin@gmail.com>
2023-02-24 14:10:06 -0500

    image optimization

--- components/Project/ProjectPage.tsx
+++ components/Project/ProjectPage.tsx
@@ -228,7 +228,7 @@ export const ProjectPage = ({ data }) => {
                   }}
                   transition={{ duration: 0.6, ease: [1, 0.15, 0.25, 0.9] }}
                 >
-                  <Image src={img} placeholder={index > 0 ? 'blur' : 'empty'} blurDataURL={lqip} alt={image.alt} layout="fill" loading={index === 0 ? 'eager' : 'lazy'} objectFit="contain" priority={index === 0} sizes="(min-width: 768px) 70vw, 100vw" />
+                  <Image src={img} placeholder={index > 0 ? 'blur' : 'empty'} blurDataURL={lqip} alt={image.alt} layout="fill" priority={index === 0} objectFit="contain" priority={index === 0} sizes="(min-width: 768px) 70vw, 100vw" />
                 </StyledImage>
               )
             })}
