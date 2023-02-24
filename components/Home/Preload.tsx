74abd6416d701a08125ca67697ed683334b324fd
Logan Sparlin <logan.sparlin@gmail.com>
2023-02-24 11:44:06 -0500

    only priority for first image

--- components/Home/Preload.tsx
+++ components/Home/Preload.tsx
@@ -6,8 +6,8 @@ export const Preload = ({ projects }) => {
     <div style={{ visibility: 'hidden', position: 'absolute', pointerEvents: 'none' }} aria-hidden={true}>
       {projects.map((project) => {
         const img = project.images?.length > 1
-          ? urlFor(project.images[0]).auto('format').width(1200).dpr(2).quality(90).url()
-          : urlFor(project.image.src).auto('format').width(1200).dpr(2).quality(90).url();
+          ? urlFor(project.images[0]).auto('format').width(1000).dpr(2).quality(90).url()
+          : urlFor(project.image.src).auto('format').width(1000).dpr(2).quality(90).url();
         return (
           <Image src={img} width={1200} height={800} alt="" aria-hidden={true} key={project._id} loading="lazy" />
         )
